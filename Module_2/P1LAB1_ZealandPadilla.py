4f92446c1cf9ed5c323d5b1830d21deab98a59ed
zlndpadilla <zlndpadilla@gmail.com>
2026-09-15 13:31:43 -0400

    Add files via upload

---
+++ Module_2/P1LAB1_ZealandPadilla.py
@@ -0,0 +1,10 @@
+# Zealand Padilla
+# 9/10/2026
+# P1LAB1
+# This project will accept input and print the output
+
+fname = input ("Enter your first name: ")
+
+lname = input ("Enter your last name: ")
+
+print("Hello, " + fname + " " + lname + "! Welcome to CTI-110")
